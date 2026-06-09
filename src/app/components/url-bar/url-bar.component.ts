ba2947b6752d4bc22096ea83fc60c5ec706b2c5d
Mike Nizinski <mike.nizinski@gmail.com>
2026-06-09 18:01:16 -0400

    feat: add UrlBarComponent with sticky display, copy, and full-URL popover

---
+++ src/app/components/url-bar/url-bar.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-url-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './url-bar.component.html',
+  styleUrls: ['./url-bar.component.css'],
+})
+export class UrlBarComponent {
+  @Input() set fullUrl(val: string) {
+    const idx = val.indexOf('?');
+    this.baseUrl = idx > -1 ? val.substring(0, idx) : val;
+    this.queryString = idx > -1 ? val.substring(idx) : '';
+    this.fullUrlValue = val;
+  }
+  @Output() urlCopied = new EventEmitter<void>();
+
+  baseUrl = '';
+  queryString = '';
+  fullUrlValue = '';
+
+  copied = false;
+  showPopover = false;
+
+  async copy(): Promise<void> {
+    await navigator.clipboard.writeText(this.fullUrlValue);
+    this.copied = true;
+    this.urlCopied.emit();
+    setTimeout(() => (this.copied = false), 1800);
+  }
+
+  togglePopover(): void {
+    this.showPopover = !this.showPopover;
+  }
+
+  closePopover(): void {
+    this.showPopover = false;
+  }
+}
